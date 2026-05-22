22f6e90ec188aa0328b1256b73ebfc35bf7129a6
Moziful Haque <mhmoni2310@gmail.com>
2026-05-22 17:48:32 +0600

    Added all pets route and featured pets route

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
+cors = require('cors');
 
+app.use(cors());
+app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -17,16 +20,32 @@ const client = new MongoClient(uri, {
     }
 });
 
-async function run() {
+const run = async () => {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const database = client.db("PetBuddyServer");
+        const allPetsCollection = database.collection("AllPets");
+
+        app.get('/all-pets', async (req, res) => {
+            const cursor = allPetsCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+        app.get("/featured-pets", async (req, res) => {
+            const pets = await allPetsCollection.find().limit(6).toArray();
+            res.send(pets);
+        });
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
